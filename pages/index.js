57cd9786612a0ff47bffd152f4e58e0ba3a13f0a
Takuya Kawai <tksepii@gmail.com>
2022-07-10 12:52:44 +0000

    index修正

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,16 @@ export default function Home() {
 
   // ロード中
   if (loading === 'loading') {
-    return <div>Loading...</div>
+    return (
+      <div className='text-center'>
+        <Head
+          title={'Next Body Composition'}
+        />
+        <h1 className='px-2 py-1 text-gray-800 text-xl font-bold'>Next Body Composition</h1>
+        <button className='px-2 py-1 bg-blue-400 text-white font-semibold rounded hover:bg-blue-500' onClick={() => signIn()}><a>ログイン</a></button>
+        <button className='px-2 py-1 bg-orange-400 text-white font-semibold rounded hover:bg-orange-500' onClick={() => router.push("/user/register")}><a>新規登録</a></button>
+      </div>
+    )
   }
 
   if (session) {
