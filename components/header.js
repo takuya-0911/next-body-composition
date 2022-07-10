15729a59186a0b2ea89dae6b6a73463c9c0289c8
Takuya Kawai <tksepii@gmail.com>
2022-07-10 07:13:14 +0000

    headerエラー改修

--- components/header.js
+++ components/header.js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 
 const Header = () => {
     const { data: session } = useSession();
-    const router = useRouter();
 
     if (session) {
         return (
@@ -16,13 +15,13 @@ const Header = () => {
                         <div>
                             <ul class="flex mt-4 flex-row space-x-8 md:mt-1 text-sm font-medium">
                                 <li>
-                                <a href="/" class="bg-transparent text-blue-500 p-0 dark:text-white" aria-current="page">トップ</a>
+                                <a href="/" className="bg-transparent text-blue-500 p-0 dark:text-white" aria-current="page">トップ</a>
                                 </li>
                                 <li>
-                                <a href="/user/update" class="hover:bg-transparent border-0 hover:text-blue-700 p-0 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 dark:hover:bg-transparent dark:border-gray-700">設定</a>
+                                <a href="/user/update" className="hover:bg-transparent border-0 hover:text-blue-700 p-0 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 dark:hover:bg-transparent dark:border-gray-700">設定</a>
                                 </li>
                                 <li>
-                                <a onClick={() => signOut()} href="#" class="hover:bg-transparent border-0 hover:text-red-500 p-0 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-red-700">サインアウト</a>
+                                <a onClick={() => signOut()} href="#" className="hover:bg-transparent border-0 hover:text-red-500 p-0 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-red-700">サインアウト</a>
                                 </li>
                             </ul>
                         </div>
