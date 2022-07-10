fe7fd11d2c8a5694697eafc9aa9094d9aa6bd775
Takuya Kawai <tksepii@gmail.com>
2022-07-10 07:29:29 +0000

    Layout小文字を大文字に

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-// import '../styles/globals.css'
 import 'tailwindcss/tailwind.css';
 import { SessionProvider } from 'next-auth/react';
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
